c7e98b60b81dbea64e159a36f998944538f67263
bixorko <peter.vinarcik99@gmail.com>
2020-04-25 15:58:09 +0200

    Project Done, need to control it on reference machine

--- PacketSniffer/Program.cs
+++ PacketSniffer/Program.cs
@@ -214,7 +214,7 @@ namespace PacketSniffer
                 Console.WriteLine("{0}:{1}:{2}.{3} {4} : {5} > {6} : {7}\n",
                     time.Hour, time.Minute, time.Second, time.Millisecond, hostnameSrc, srcPort, hostnameDst, dstPort);
                 
-                var filteredData = TakeNeccesaryDatas(tcpPacket.PrintHex());
+                var filteredData = TakeNeccesaryDatas(udpPacket.PrintHex());
                 filterDataAndPrintIt(filteredData);
             }
             
